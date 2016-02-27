44e7ce3df338ce2b023b934027cb953648e2ae44
mateusz <mateusz.jonak@gmail.com>
2016-02-27 14:36:03 +0100

    Separate product.json and add filter func

--- src/components/productsSite.ts
+++ src/components/productsSite.ts
@@ -1,4 +1,5 @@
-import {Component, View} from 'angular2/core';
+import { Component, View } from 'angular2/core';
+import { Control } from 'angular2/common'
 
 import { ProductsListComponent } from "./productsList";
 import { Product } from '../models/product.entity';
@@ -12,32 +13,36 @@ import { Product } from '../models/product.entity';
     directives: [ProductsListComponent]
 })
 export class ProductsSiteComponent {
-    public products = [
-        new Product({
-            id: 1,
-            name: 'item1',
-            price: 20,
-            done: false,
-            tags: ['toys']
-        }),
-        new Product({
-            id: 2,
-            name: 'item2',
-            price: 40,
-            done: false,
-            tags: ['sport']
-        }),
-        new Product({
-            id: 3,
-            name: 'item3',
-            price: 60,
-            done: false,
-            tags: ['sport', 'toys']
-        })
+    public searchInput = new Control();
+    public _products = [
+        new Product(require('./products.json')[0]),
+        new Product(require('./products.json')[1]),
+        new Product(require('./products.json')[2])
     ];
 
+    get products () {
+        return this._products.filter((product) => {
+            let filterVal:string = this.searchInput.value || '';
+            let regexp:any;
+
+            regexp = new RegExp(filterVal, 'ig');
+
+            let booleans = {
+                name: false,
+                tags: false
+            };
+
+            booleans.name = product.name.search(regexp) !== -1;
+            booleans.tags = product.tags.filter(tag => {
+                return tag.search(regexp) !== -1;
+            }).length > 0;
+
+            return booleans.name || booleans.tags;
+        });
+    }
+
     get specialProducts () {
-        return this.products.filter((product) => {
+        return this._products.filter((product) => {
             return product.id === 1;
         });
     }
